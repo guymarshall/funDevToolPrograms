9111d6773c3b10f25f3fc40d55a11a57a8ddd13d
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-03-30 12:36:18 +0100

    Change var to let

--- fa-dazzle.js
+++ fa-dazzle.js
@@ -1,7 +1,7 @@
 setInterval(function() {
-    var elements = document.getElementsByTagName("*");
-    for (var i = 0; i < elements.length; i++) {
-        var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    let elements = document.getElementsByTagName("*");
+    for (let i = 0; i < elements.length; i++) {
+        let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
         elements[i].style.backgroundColor = randomColor;
     }
 }, 1000);
