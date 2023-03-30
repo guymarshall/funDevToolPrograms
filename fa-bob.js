e59816f2c7692c019d23d76511122f2f4b289ff3
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-03-30 12:26:03 +0100

    Remove comments

--- fa-bob.js
+++ fa-bob.js
@@ -1,10 +1,10 @@
 function getRandomSize() {
-  return Math.floor(Math.random() * 100) + 50; // Returns a random number between 50 and 150
+  return Math.floor(Math.random() * 100) + 50;
 }
 
 function animateElement(element) {
     const size = getRandomSize();
-    const duration = Math.floor(Math.random() * 1000) + 500; // Returns a random duration between 500 and 1500 ms
+    const duration = Math.floor(Math.random() * 1000) + 500;
     element.animate([
         { transform: `scale(1)` },
         { transform: `scale(${size / 100})` },
@@ -18,5 +18,5 @@ function animateElement(element) {
 document.querySelectorAll('*').forEach(element => {
     setInterval(() => {
         animateElement(element);
-    }, Math.floor(Math.random() * 2000) + 500); // Returns a random interval between 500 and 2500 ms
+    }, Math.floor(Math.random() * 2000) + 500);
 });
