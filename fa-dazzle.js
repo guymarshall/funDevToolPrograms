08a54194b0c18d47e9ff8236fc60375c5b04b379
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-03-30 12:27:34 +0100

    Add fa-dazzle.js

---
+++ fa-dazzle.js
@@ -0,0 +1,7 @@
+setInterval(function() {
+    var elements = document.getElementsByTagName("*");
+    for (var i = 0; i < elements.length; i++) {
+        var randomColor = Math.floor(Math.random()*16777215).toString(16);
+        elements[i].style.color = "#" + randomColor;
+    }
+}, 100);
