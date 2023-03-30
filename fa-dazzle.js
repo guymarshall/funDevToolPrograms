8591ba2ce3c4f80e16271c7167632acd4baae885
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-03-30 12:35:52 +0100

    Upgrade fa-dazzle to use random hex values instead of predetermined array of colours

--- fa-dazzle.js
+++ fa-dazzle.js
@@ -1,9 +1,7 @@
 setInterval(function() {
-    let elements = document.getElementsByTagName("*");
-    let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-    let index = 0;
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].style.backgroundColor = colors[index % colors.length];
-        index++;
+    var elements = document.getElementsByTagName("*");
+    for (var i = 0; i < elements.length; i++) {
+        var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+        elements[i].style.backgroundColor = randomColor;
     }
 }, 1000);
